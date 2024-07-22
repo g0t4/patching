3fb47828d5aaf3de17c85a500ae430b0c6ef771b
Wes Higbee <wes.mcclure@gmail.com>
2024-07-22 13:49:32 -0500

    diff examples

--- ideas/commands.fish
+++ ideas/commands.fish
@@ -1,1 +1,7 @@
 sed -i '' 's/Higbee/Mcclure/g' change-name/catalog.html
+
+diff catalog.html change-name/catalog.html > ideas/diff-no-args.patch
+diff -u catalog.html change-name/catalog.html > ideas/diff-u.patch
+diff -c catalog.html change-name/catalog.html > ideas/diff-c.patch
+diff -e catalog.html change-name/catalog.html > ideas/diff-e.patch
+
