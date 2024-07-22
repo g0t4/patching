e3314112b162b2ffc224dd673d8d6202d02f06f9
Wes Higbee <wes.mcclure@gmail.com>
2024-07-22 14:09:02 -0500

    commands

--- ideas/commands.fish
+++ ideas/commands.fish
@@ -5,3 +5,7 @@ diff -u catalog.html change-name/catalog.html > ideas/diff-u.patch
 diff -c catalog.html change-name/catalog.html > ideas/diff-c.patch
 diff -e catalog.html change-name/catalog.html > ideas/diff-e.patch
 
+# patching
+patch catalog.html < ideas/diff-no-args.patch
+git status/diff # run after to show diff
+
